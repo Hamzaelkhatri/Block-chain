1062c2e5ab7e1756f23c8e037c487d2e39cf5eb2
Hamza elkhatri <40549481+Hamzaelkhatri@users.noreply.github.com>
2022-10-27 00:36:35 +0100

    Update getAccount.js

--- quest4/getAccount.js
+++ quest4/getAccount.js
@@ -1,18 +1,8 @@
-const ethers = require('ethers');
-const getAccount = require('./getAccount');
-
-
-async function sendEther(amount, address) {
+async function getAccount() {
+    const ethers = require('ethers');
     const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545");
-    const accounts = await getAccount()
-    const signer = provider.getSigner(accounts);
-    const tx = await signer.sendTransaction({
-        to: address,
-        value: ethers.utils.parseEther(amount.toString())
-        
-    });
-    await tx.wait();
-    return tx;
+    // return the first account
+    const accounts = await provider.listAccounts();
+    return accounts[0];
 }
-
-module.exports = sendEther
+module.exports = getAccount
