1831d914e69a0caba2933877ebb2ad4100b3711d
Hamza elkhatri <40549481+Hamzaelkhatri@users.noreply.github.com>
2022-10-26 01:39:50 +0100

    Update generateaddress.js

--- quest2/generateaddress.js
+++ quest2/generateaddress.js
@@ -1,28 +1,26 @@
-/*
-Create a function generateAddress() that generates a pair of cryptographic keys using the elliptic curve secp256k1. The return value is an object with members:
-
-privateKey, a private key as a string (pkcs8/pem format)
-publicKey, the corresponding public key as a string (spki/pem)
-address, an address to identify this account. The address is the hash sha256 of the publicKey (in format spki/der) prepended with '01'
-*/
-
-const { createHash } = require("crypto")
-const { createECDH, createPublicKey, createPrivateKey } = require("crypto")
+var crypto = require('crypto');
 
 
-const generateAddress = () => {
+function generateAddress() {
 
-    const ec = createECDH('secp256k1')
-    ec.generateKeys()
+    const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
+        namedCurve: 'secp256k1',
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem'
+        }
+    });
 
-    const privateKey = ec.getPrivateKey('hex')
-    const publicKey = ec.getPublicKey('hex')
-
-    const address = createHash('sha256').update(publicKey, 'hex').digest('hex')
+    const address = crypto.createHash('sha256').update(publicKey).digest('hex')
 
     return {
         privateKey,
         publicKey,
-        address
+        address: '01' + address
     }
 }
+  
